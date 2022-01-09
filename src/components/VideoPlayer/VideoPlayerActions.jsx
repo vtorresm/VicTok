3bf9ccf61a83efb82e2feeb2215fe7346869f1f9
vtorresm <vatorresm88@gmail.com>
2022-01-09 18:35:27 -0500

    Adding heart, share and comment icons

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,20 +1,24 @@
+
 import { Heart } from '../Icons/Heart.jsx'
-// import { Share } from '../Icons/Share.jsx'
-// import { Comment } from '../Icons/Comment.jsx'
+import { Share } from '../Icons/Share.jsx'
+import { Comment } from '../Icons/Comment.jsx'
 
 import styles from './styles.module.css'
 
-export default function VideoPlayerActions () {
+export default function VideoPlayerActions (likes = 2022, shares = 333, comments = 678, hearted = false) {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <span title='like'>{likes}</span>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Comment />
+        <span title='comments'>{comments}</span>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <span title='shares'>{shares}</span>
       </div>
     </aside>
   )
