1af8d7dd5b10b0831b597652d0f99aec57ea0651
vtorresm <vatorresm88@gmail.com>
2022-01-10 14:37:08 -0500

    Testing the buttons

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,25 +1,43 @@
-
 import { Heart } from '../Icons/Heart.jsx'
 import { Share } from '../Icons/Share.jsx'
 import { Comment } from '../Icons/Comment.jsx'
 
 import styles from './styles.module.css'
 
-export default function VideoPlayerActions (likes = 2022, shares = 333, comments = 678, hearted = false) {
+export default function VideoPlayerActions ({
+  likes = 2022,
+  shares = 333,
+  comments = 678,
+  hearted = false
+}) {
+  const handleLike = () => {
+    window.alert('like')
+  }
+
+  const handleComment = () => {
+    window.alert('comment')
+  }
+
+  const handleShare = () => {
+    window.alert('share')
+  }
+
   return (
     <aside className={styles.actions}>
-      <div className={styles.action}>
+      <button onClick={handleLike} className={styles.action}>
         <Heart />
         <span title='like'>{likes}</span>
-      </div>
-      <div className={styles.action}>
+      </button>
+
+      <button onClick={handleComment} className={styles.action}>
         <Comment />
         <span title='comments'>{comments}</span>
-      </div>
-      <div className={styles.action}>
+      </button>
+
+      <button onClick={handleShare} className={styles.action}>
         <Share />
         <span title='shares'>{shares}</span>
-      </div>
+      </button>
     </aside>
   )
 }
