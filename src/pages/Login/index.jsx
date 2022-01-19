eeb82548ae7dba48287b0697f094a0e33b0fcf00
vtorresm <vatorresm88@gmail.com>
2022-01-18 21:37:42 -0500

    adding page Login

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,17 @@
+import { supabase } from '@src/services/supabase.js'
+
+export default function Login () {
+  const handleClick = async () => {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'twitter'
+    })
+
+    console.log({ user, session, error })
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Login with Twitter</button>
+    </div>
+  )
+}
