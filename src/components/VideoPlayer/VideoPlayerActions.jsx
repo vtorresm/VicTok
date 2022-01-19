1f688acef6174ac009aa3cf668422dec08db841d
vtorresm <vatorresm88@gmail.com>
2022-01-18 21:52:50 -0500

    Vidoe Player

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -4,12 +4,7 @@ import { Comment } from '../Icons/Comment.jsx'
 
 import styles from './styles.module.css'
 
-export default function VideoPlayerActions ({
-  likes = 2022,
-  shares = 333,
-  comments = 678,
-  hearted = false
-}) {
+export default function VideoPlayerActions ({ username, avatar, likes = 2041, comments = 333, shares = 520, hearted = false }) {
   const handleLike = () => {
     window.alert('like')
   }
@@ -24,18 +19,24 @@ export default function VideoPlayerActions ({
 
   return (
     <aside className={styles.actions}>
+
+      <div className={styles.user}>
+        <img alt={username} src={avatar} />
+        <img src='https://sf16-scmcdn-va.ibytedtos.com/goofy/tiktok/web/node/_next/static/images/test-2e6dd40439e72f09a8193e27cb3e0c51.svg' width='24' />
+      </div>
+
       <button onClick={handleLike} className={styles.action}>
-        <Heart />
+        <Heart width='45' />
         <span title='like'>{likes}</span>
       </button>
 
       <button onClick={handleComment} className={styles.action}>
-        <Comment />
+        <Comment width='45' />
         <span title='comments'>{comments}</span>
       </button>
 
       <button onClick={handleShare} className={styles.action}>
-        <Share />
+        <Share width='45' />
         <span title='shares'>{shares}</span>
       </button>
     </aside>
--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,21 +1,19 @@
 import clsx from 'clsx'
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 import styles from './styles.module.css'
-import VideoPlayerActions from './VideoPlayerActions'
+import VideoPlayerActions from './VideoPlayerActions.jsx'
+import VideoDescription from '../VideoDescription/index.jsx'
+import useIntersectionVideoPlayer from '../../hooks/useIntersectionVideoPlayer.js'
 
-export default function VideoPlayer ({ src }) {
+export default function VideoPlayer (props) {
   const video = useRef(null)
-  const [playing, setPlaying] = useState(false)
-
-  const handlePlay = () => {
-    const { current: videoEl } = video
-    playing ? videoEl.pause() : videoEl.play()
-    setPlaying(!playing)
-  }
+  const { playing, handlePlay } = useIntersectionVideoPlayer({ video })
 
   const playerClassName = clsx(styles.player, {
     [styles.hidden]: playing
   })
+
+  const { src } = props
 
   return (
     <div className={styles.wrapper}>
@@ -28,7 +26,9 @@ export default function VideoPlayer ({ src }) {
         src={src}
       />
       <i className={playerClassName} onClick={handlePlay} />
-      <VideoPlayerActions />
+      <VideoPlayerActions {...props} />
+      <VideoDescription {...props} />
     </div>
+
   )
 }
