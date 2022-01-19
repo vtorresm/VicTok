34052906f1385f78f22e93535c5e428e92578415
vtorresm <vatorresm88@gmail.com>
2022-01-18 21:56:42 -0500

    global modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
-import FeedVideos from './components/FeedVideos/index'
+import { lazy, Suspense } from 'react'
+import { Route } from 'wouter'
+
+const Home = lazy(() => import('./pages/Home/index.jsx'))
+const Upload = lazy(() => import('./pages/Upload/index.jsx'))
+const Login = lazy(() => import('./pages/Login/index.jsx'))
 
 function App () {
   return (
     <div className='App'>
       <main>
-        <FeedVideos />
+        <Suspense fallback={<div />}>
+          <Route path='/'>
+            <Home />
+          </Route>
+
+          <Route path='/upload'>
+            <Upload />
+          </Route>
+
+          <Route path='/login'>
+            <Login />
+          </Route>
+        </Suspense>
       </main>
     </div>
   )
